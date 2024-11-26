cd56059f1ff3d1bd5fa1ef95fc345c0fa2a39bb3
Seunghwan8334 <seunghwan8334@gmail.com>
2024-11-26 23:05:17 +0900

    Save changes before rebase

--- commands_set/user_prefix_commands.py
+++ commands_set/user_prefix_commands.py
@@ -1,12 +1,38 @@
-from initializations import bot
+from initializations import *
 
 @bot.command(name="test") 
 async def test(ctx):
     await ctx.send("정상 가동 중..")
 
-@bot.command(name="server")
+@bot.command(name="invite") 
+async def invite_link(ctx):
+    await ctx.send(discordServerLink)
+
+@bot.command(name="profile")
+async def send_embed(ctx, member:discord.Member = None):
+    if member:
+        user = member
+    else: 
+        user = ctx.author
+
+    embed = discord.Embed(
+        title=f"{user.name}'s profile",
+        description="This is the description of your profile",
+        color=discord.Color.blue()
+    )
+
+    embed.set_thumbnail(url=user.avatar.url if user.avatar else user.default_avatar.url)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Nickname", value=user.display_name, inline=True)
+    embed.add_field(name="Account Created", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+    embed.add_field(name="Joined Server", value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S") if user.joined_at else "N/A", inline=False)
+    embed.set_footer(text="Your Discord Profile")
+
+    await ctx.send(embed=embed)
+
+@bot.command(name="code") 
 async def server(ctx):
-    await ctx.send(ctx.guild)
+    await ctx.send(discordServerLink)
 
 @bot.command(name="add") 
 async def add(ctx, a: int,b: int):
--- initializations.py
+++ initializations.py
@@ -1,9 +1,13 @@
 import discord
 from discord.ext import commands
+
 import os 
 from dotenv import load_dotenv 
 load_dotenv() 
+
+discordServerLink = os.getenv("DISCORD_SERVER_LINK")
 bot_token = os.getenv("DISCORD_BOT_TOKEN") 
+
 
 intents = discord.Intents.default()
 intents.messages = True
